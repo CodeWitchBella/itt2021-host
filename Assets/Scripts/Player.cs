2b9c3eae87a8735ebb8f406fa85a6a8db2ebe2bb
Isabella Skořepová <isabella@skorepova.info>
2021-04-23 03:04:33 +0200

    enable OpenXR

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Normal.Realtime;
+
+public class Player : MonoBehaviour
+{
+    private Realtime _realtime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _realtime = FindObjectOfType<Realtime>();
+        _realtime.didConnectToRoom += didConnectToRoom;
+    }
+
+    private void didConnectToRoom(Realtime realtime)
+    {
+        var player = Realtime.Instantiate("Player",
+            position: Vector3.zero,
+            rotation: Quaternion.identity,
+            ownedByClient: true,
+            preventOwnershipTakeover: false,
+            destroyWhenOwnerOrLastClientLeaves: true,
+            useInstance: realtime
+        );
+    }
+}
