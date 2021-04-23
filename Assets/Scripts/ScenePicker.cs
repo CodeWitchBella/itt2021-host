c2161f9719a7c503ced86d74192c6441e870d6ac
Isabella Skořepová <isabella@skorepova.info>
2021-04-23 04:54:59 +0200

    refactor to allow direct VR scene loading

--- Assets/Scripts/ScenePicker.cs
+++ Assets/Scripts/ScenePicker.cs
@@ -6,7 +6,7 @@ using UnityEngine.XR;
 using UnityEngine.XR.Management;
 using UnityEngine.InputSystem;
 
-public class XRLoader : MonoBehaviour
+public class ScenePicker : MonoBehaviour
 {
     public bool VREnabled = true;
 
@@ -34,12 +34,6 @@ public class XRLoader : MonoBehaviour
 
         if (HasVR())
         {
-
-            List<XRDisplaySubsystemDescriptor> displays = new List<XRDisplaySubsystemDescriptor>();
-            SubsystemManager.GetSubsystemDescriptors(displays);
-            Debug.Log("Number of display providers found: " + displays.Count);
-
-
             Debug.Log("Press V to load into VR, or D to load into desktop mode");
             bool v = false;
             bool d = false;
@@ -49,57 +43,25 @@ public class XRLoader : MonoBehaviour
                 d = Keyboard.current.dKey.wasReleasedThisFrame;
                 yield return null;
             }
+
             Debug.Log("V: " + (v ? "Yes" : "No") + " D: " + (d ? "Yes" : "No"));
-            if (!v) Debug.Log("Skipping into desktop mode");
+            if (d) Debug.Log("Desktop mode selected");
             else
             {
                 Debug.Log("VR mode selected");
-
-                Debug.Log("Initializing XR");
-                yield return XRGeneralSettings.Instance.Manager.InitializeLoader();
-                while (!XRGeneralSettings.Instance.Manager.isInitializationComplete) yield return null;
-
-                Debug.Log("Starting XR subsystems");
-                XRGeneralSettings.Instance.Manager.StartSubsystems();
-
-                foreach (var display in displays)
-                {
-                    if (display.id.Contains("MockHMD") || !VREnabled) continue;
-
-                    Debug.Log("Creating display " + display.id);
-                    XRDisplaySubsystem dispInst = display.Create();
-
-                    if (dispInst != null)
-                    {
-                        Debug.Log("Starting display " + display.id);
-                        dispInst.Start();
-                        while (!dispInst.running) yield return null;
-                        Debug.Log("Loading VR scene");
-                        SceneManager.LoadScene(sceneNameBase + " VR", LoadSceneMode.Single);
-                        vrLoaded = true;
-                        break;
-                    }
-                    else Debug.Log("Display instance is null");
-                }
+                SceneManager.LoadScene(sceneNameBase + " VR", LoadSceneMode.Single);
+                vrLoaded = true;
             }
         }
 
         if (!vrLoaded)
-        {
             SceneManager.LoadScene(sceneNameBase + " PC", LoadSceneMode.Single);
-        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine("InitXR");
-        DontDestroyOnLoad(this);
     }
 
-    void OnDestroy()
-    {
-        if (XRGeneralSettings.Instance.Manager.isInitializationComplete)
-            XRGeneralSettings.Instance.Manager.DeinitializeLoader();
-    }
 }
