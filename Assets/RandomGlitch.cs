2a37394671ddc844519adb9160361259aa81e44f
Viktor Bobůrka <boburvik@fel.cvut.cz>
2021-04-26 22:05:41 +0200

    script to trigger glitch

--- Assets/RandomGlitch.cs
+++ Assets/RandomGlitch.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomGlitch : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space)) {
+            foreach (GameObject g in GameObject.FindGameObjectsWithTag("Glitchable")) {
+                g.GetComponent<MeshRenderer>().material = (Material)Resources.Load("TestMaterial", typeof(Material));
+            }
+        }
+        else {
+            foreach (GameObject g in GameObject.FindGameObjectsWithTag("Glitchable")) {
+                g.GetComponent<MeshRenderer>().material = (Material)Resources.Load("NormalMaterial", typeof(Material));
+            }
+        }
     }
+    /*private bool isObjectVisible(GameObject obj) {
+    Plane[] planes = GeometryUtility.CalculateFrustumPlanes(currCam);
+      if (GeometryUtility.TestPlanesAABB(planes, obj.collider.bounds))
+          return true;
+      else
+          return false;
+    }*/
 }
