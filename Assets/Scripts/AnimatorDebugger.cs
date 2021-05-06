283a220c3bf85725f9401aee833e001e7e97cc14
Isabella Skořepová <isabella@skorepova.info>
2021-05-07 01:21:50 +0200

    position pruchod

--- Assets/Scripts/AnimatorDebugger.cs
+++ Assets/Scripts/AnimatorDebugger.cs
@@ -2,25 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class SpeedUpInEditor : MonoBehaviour
+public class AnimatorDebugger : MonoBehaviour
 {
+    private float speed;
+    Animator animator;
     void Start()
     {
         if (Application.isEditor) {
+            animator = GetComponent<Animator>();
+
             // If you want to load speed up animations in editor just add
             // speed.txt with one line to Assets
             var file = Application.dataPath + "/speed.txt";
             if (System.IO.File.Exists(file)) {
                 var lines = System.IO.File.ReadAllLines(file);
-                float speed;
                 if (float.TryParse(lines[0], out speed)) {
-                    var animator = GetComponent<Animator>();
                     if (animator) animator.speed = speed;
                 }
             }
+        } else {
+            Destroy(this);
         }
-        Destroy(this);
     }
 
+    void Update()
+    {
+        if (!Application.isEditor) { Destroy(this); return; }
+        if (Keyboard.current.spaceKey.wasReleasedThisFrame) {
+            if (animator) {
+                if (animator.speed > 0.01) animator.speed = 0;
+                else animator.speed = speed;
+            }
+        }
+    }
 }
