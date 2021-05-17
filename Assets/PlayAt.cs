9031c37ba66bd8312d00f93976c2a599fb83d2ef
Viktor Bobůrka <boburvik@fel.cvut.cz>
2021-05-17 13:57:11 +0200

    music added

---
+++ Assets/PlayAt.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAt : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float playTime;
+    private bool playing = false;
+    private AudioSource asrc;
+    void Start()
+    {
+        asrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= playTime && !playing) {
+            asrc.Play();
+            playing = true;
+        }
+    }
+}
