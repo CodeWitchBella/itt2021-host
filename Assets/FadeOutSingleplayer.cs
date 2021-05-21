130827725b13f78ccd4a01388d88978f6f75d67d
viktorboburka <viktor@boburka.cz>
2021-05-21 14:29:24 +0200

    fadeout singleplayer

---
+++ Assets/FadeOutSingleplayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOutSingleplayer : MonoBehaviour
+{
+
+    private RawImage img;
+    private float alpha;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 3;
+        alpha = 0.0f;
+        img = gameObject.GetComponent<RawImage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeSinceLevelLoad > 307) {
+            alpha += (Time.deltaTime / 5.0f);
+        }
+        gameObject.GetComponent<RawImage>().color = new Color(1.0f, 1.0f, 1.0f, alpha);
+    }
+}
---
+++ Assets/FadeoutMultiplayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeoutMultiplayer : MonoBehaviour
+{
+
+    private RawImage img;
+    private float alpha;
+    private GameObject waitForStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alpha = 0.0f;
+        img = gameObject.GetComponent<RawImage>();
+        waitForStart = GameObject.Find("WaitForStart");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waitForStart.GetComponent<WaitForStart>().timeRemaining < 91) {
+            alpha += (Time.deltaTime / 90.0f);
+            gameObject.GetComponent<RawImage>().color = new Color(0.0f, 0.0f, 0.0f, alpha);
+        }
+    }
+}
